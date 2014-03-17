4b86a979facf1dc0bae2ff31d7ba14c19d337a09
Can Liu <liucan@bobac.soic.indiana.edu>
2014-03-16 21:09:07 -0400

    with some questions

--- pmi/pmi.py
+++ pmi/pmi.py
@@ -18,7 +18,23 @@ class PMI:
         print("Pickles for {} has been loaded!".format(lang))
 
     def sim_wrd_ea(self):  ##similarity based on dependency of words
-        pass
+        ##handle special cases, if some counts are zero, there will be lookup error
+        ##Question:::  lowercases????
+
+        try:
+            num = self.pos_dep[(head,dep,label)]
+        except:
+            num = 0
+        try:
+            demon1 = self.pos_all[(head,label,"head")]
+        except:
+            demon1 = 0
+        try:
+            demon2 = self.pos_all(dep,label,"dep")
+        except:
+            demon2 = 0
+        if (demon1 + demon2) == 0: return 0
+        else: return num/ (demon1 + demon2)
 
     def sim_pos_ea(self,head,dep,label):
 
@@ -40,7 +56,7 @@ class PMI:
         if (demon1 + demon2) == 0: return 0
         else: return num/ (demon1 + demon2)
 
-    def sim_pos(self,deplist,lang):  ## the input is a list of dependencies.
+    def sim_pos(self,deplist,lang,mode):  ## the input is a list of dependencies.
         ##should look up each label, and then average the result.
         length = 0  ##if there is a root for English, then ignore it. 
         sim_sum = 0
@@ -49,7 +65,10 @@ class PMI:
             if lang == "en" and label == "root":  ### XXX Question, what should this be??
                 continue
             else:
-                sim_sum += sim_pos_ea(head,dep,label)
+                if mode == "pos":
+                    sim_sum += sim_pos_ea(head,dep,label)
+                else:
+                    sim_sum += sim_wrd_ea(head,dep,label)
                 length +=1
 
         return sim_sum/length
