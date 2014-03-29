103f26fa819edbad28ce18aeff4ae09c9ba6ef0c
Alex Rudnick <alex.rudnick@gmail.com>
2014-03-29 00:52:44 -0400

    better candidate generation, do lookups for phrase tables from sqlite too

---
+++ firstcut/run-en-es.sh
@@ -0,0 +1,8 @@
+python3 firstcut.py \
+  --source en \
+  --target es \
+  --infn ../trialdata/en-es.trial.xml \
+  --outfn ./theoutput \
+  --lm /space/spanish-wikipedia/spanish.blm \
+  --pt /space/phrasetables-db/en-es.db \
+  --weights SAMPLE.weights
