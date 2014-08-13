f4beddfbd479b8e3096ec76216bab44bd0409ee3
Levi King <leviaking@gmail.com>
2014-08-12 22:25:15 -0400

    LK: trying again...

--- firstcut/setup_virtualenv.sh
+++ firstcut/setup_virtualenv.sh
@@ -3,7 +3,7 @@
 ## you may also need:
 ## sudo apt-get install libxslt1-dev
 
-virtualenv -p /usr/bin/python3 venv
+virtualenv -p /opt/local/bin/python3.3 venv
 . venv/bin/activate
 pip install https://github.com/kpu/kenlm/archive/master.zip
 pip install lxml
